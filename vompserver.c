a4da94e39bf78d16dc4350cba069948a6a525241
Chris Tallon <chris@vomp.tv>
2007-02-04 23:22:44 +0000

    Version number

--- vompserver.c
+++ vompserver.c
@@ -23,7 +23,7 @@
 
 #include "mvpserver.h"
 
-static const char *VERSION        = "0.2.5";
+static const char *VERSION        = "0.2.6";
 static const char *DESCRIPTION    = "VDR on MVP plugin by Chris Tallon";
 
 class cPluginVompserver : public cPlugin
